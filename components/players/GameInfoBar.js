2e4aecba8c0bcb7973741c56b538ddf78c3e43d3
Rúben Sousa <rubensousa96@hotmail.com>
2024-05-25 10:41:06 +0100

    added more info to the game bars

--- components/players/GameInfoBar.js
+++ components/players/GameInfoBar.js
@@ -6,9 +6,7 @@ import Image from "next/image";
 import { Tooltip } from "react-tooltip";
 
 const GameInfoBar = ({ data }) => {
-
   console.log(data);
-
   const dateFromTimestamp = new Date(data.game_end_timestamp);
   const currentDate = new Date();
   const timeDifference = currentDate - dateFromTimestamp;
@@ -22,10 +20,8 @@ const GameInfoBar = ({ data }) => {
     "https://fpwrnfdqzvztmakmrdnc.supabase.co/storage/v1/object/public/roles/" +
     data.teamPosition.toLowerCase() +
     ".png";
-
   const sum_spell_1_str = data.summoner1Id.toString();
   const sum_spell_2_str = data.summoner2Id.toString();
-
   function findSummonerByKey(data, key) {
     for (const summoner in data) {
       if (data[summoner].key === key) {
@@ -34,27 +30,39 @@ const GameInfoBar = ({ data }) => {
     }
     return null;
   }
-
   const sum1img = findSummonerByKey(lol_spells.data, sum_spell_1_str);
   const sum2img = findSummonerByKey(lol_spells.data, sum_spell_2_str);
-
-  const summoner1 = "https://fpwrnfdqzvztmakmrdnc.supabase.co/storage/v1/object/public/sum_spells/" + sum1img;
-  const summoner2 = "https://fpwrnfdqzvztmakmrdnc.supabase.co/storage/v1/object/public/sum_spells/" + sum2img;
-
-  const items = [data.item0, data.item1, data.item2, data.item3, data.item4, data.item5, data.item6];
+  const summoner1 =
+    "https://fpwrnfdqzvztmakmrdnc.supabase.co/storage/v1/object/public/sum_spells/" +
+    sum1img;
+  const summoner2 =
+    "https://fpwrnfdqzvztmakmrdnc.supabase.co/storage/v1/object/public/sum_spells/" +
+    sum2img;
+  const items = [
+    data.item0,
+    data.item1,
+    data.item2,
+    data.item3,
+    data.item4,
+    data.item5,
+    data.item6,
+  ];
+  const kda = (data.kills + data.assists) / data.deaths;
+  const cs_per_m = data.totalMinionsKilled / data.game_time;
+  const kill_part = data.killParticipation * 100;
 
   return (
     <div
-      className={`bg-zinc-800 p-4 rounded-md border-l-2 ${data.win === false ? "border-red-500" : "border-cyan-500"
-        }`}
+      className={`bg-zinc-800 p-2 px-4 rounded-md border-l-2 ${
+        data.win === false ? "border-red-500" : "border-cyan-500"
+      }`}
     >
       <div className="flex items-center justify-between">
-        <div className="flex gap-2 mb-4">
+        <div className="flex gap-4">
           <span className="uppercase text-xs">Ranked Solo</span>
-          <span className=" text-xs text-teal-400">
+          <span className=" text-xs text-zinc-400">
             {daysDifference === 0 ? "Today" : `${daysDifference}d ago`}
           </span>
-          <span className=" text-xs text-teal-400">-</span>
           <span className=" text-xs text-teal-400">
             {data.game_time} minutes
           </span>
@@ -69,7 +77,7 @@ const GameInfoBar = ({ data }) => {
           </Link>
         </div>
       </div>
-      <div className="flex items-center gap-3 h-20">
+      <div className="flex items-center gap-3 my-2">
         <div className="w-16 h-16 overflow-hidden rounded-md border border-teal-500">
           <Image
             src={champIcon}
@@ -111,15 +119,20 @@ const GameInfoBar = ({ data }) => {
             ></Image>
           </div>
         </div>
-        <div className="block gap-3 bg-zinc-600 w-[2px] rounded-full h-full">
-        </div>
+        <div className="block gap-3 bg-zinc-600 w-[2px] rounded-full h-full"></div>
         <div className="flex flex-col items-center gap-3">
           <div className="grid grid-cols-3 gap-3">
-            {items.slice(0, 6).map((item, index) => (
+            {items.slice(0, 6).map((item, index) =>
               item !== 0 ? (
                 <div key={index}>
                   <Image
-                    src={data[`item${index}`] !== 0 ? `https://ddragon.leagueoflegends.com/cdn/14.10.1/img/item/${data[`item${index}`]}.png` : ``}
+                    src={
+                      data[`item${index}`] !== 0
+                        ? `https://ddragon.leagueoflegends.com/cdn/14.10.1/img/item/${
+                            data[`item${index}`]
+                          }.png`
+                        : ``
+                    }
                     height={30}
                     width={30}
                     alt=""
@@ -133,15 +146,21 @@ const GameInfoBar = ({ data }) => {
                   </div>
                 </div>
               )
-            ))}
+            )}
           </div>
         </div>
         <div>
-          {items.length > 6 && (
-            items[6] !== 0 ? (
+          {items.length > 6 &&
+            (items[6] !== 0 ? (
               <div key={6}>
                 <Image
-                  src={data[`item6`] !== 0 ? `https://ddragon.leagueoflegends.com/cdn/14.10.1/img/item/${data[`item6`]}.png` : ``}
+                  src={
+                    data[`item6`] !== 0
+                      ? `https://ddragon.leagueoflegends.com/cdn/14.10.1/img/item/${
+                          data[`item6`]
+                        }.png`
+                      : ``
+                  }
                   height={30}
                   width={30}
                   alt=""
@@ -154,8 +173,147 @@ const GameInfoBar = ({ data }) => {
                   -
                 </div>
               </div>
-            )
-          )}
+            ))}
+        </div>
+        <div className="flex flex-col items-center ml-4">
+          <div>
+            {data.kills} / {data.deaths} / {data.assists}
+          </div>
+          <div className="text-xs text-teal-400">
+            {parseFloat(kda.toFixed(1))} KDA
+          </div>
+        </div>
+        <div className="flex flex-col items-center ml-4">
+          <div>{data.totalMinionsKilled} CS</div>
+          <div className="text-xs text-teal-400">
+            {parseFloat(cs_per_m.toFixed(2))} CS/M
+          </div>
+        </div>
+        <div className="flex flex-col items-center ml-4">
+          <div>{kill_part.toFixed(1)}% KP</div>
+        </div>
+        <div className="flex flex-col items-center ml-4 ">
+          <div className="text-sm text-zinc-300">
+            {data.wardsKilled} Wards Killed
+          </div>
+          <div className="text-sm text-zinc-300">
+            {data.wardsPlaced} Wards Placed
+          </div>
+          <div className="text-sm text-zinc-300">
+            {data.visionScore} Vis. Score
+          </div>
+        </div>
+        <div className="flex flex-col items-center ml-4 min-w-20">
+          <div className="flex flex-col gap-1">
+            <div className="text-cyan-500 uppercase text-sm">Blue team</div>
+            <div className="flex items-center gap-2">
+              <Image
+                src={`https://fpwrnfdqzvztmakmrdnc.supabase.co/storage/v1/object/public/champions_icons/${data.blue_top.champ_name}.png`}
+                height={20}
+                width={20}
+                alt=""
+                className="border-cyan-500 bg-zinc-900 border rounded-md"
+              ></Image>
+              {data.blue_top.champ_name}
+            </div>
+            <div className="flex items-center gap-2">
+              <Image
+                src={`https://fpwrnfdqzvztmakmrdnc.supabase.co/storage/v1/object/public/champions_icons/${data.blue_jungler.champ_name}.png`}
+                height={20}
+                width={20}
+                alt=""
+                className="border-cyan-500 bg-zinc-900 border rounded-md"
+              ></Image>
+              {data.blue_jungler.champ_name}
+            </div>
+            <div className="flex items-center gap-2">
+              <Image
+                src={`https://fpwrnfdqzvztmakmrdnc.supabase.co/storage/v1/object/public/champions_icons/${data.blue_mid.champ_name}.png`}
+                height={20}
+                width={20}
+                alt=""
+                className="border-cyan-500 bg-zinc-900 border rounded-md"
+              ></Image>
+              {data.blue_mid.champ_name}
+            </div>
+            <div className="flex items-center gap-2">
+              <Image
+                src={`https://fpwrnfdqzvztmakmrdnc.supabase.co/storage/v1/object/public/champions_icons/${data.blue_bot.champ_name}.png`}
+                height={20}
+                width={20}
+                alt=""
+                className="border-cyan-500 bg-zinc-900 border rounded-md"
+              ></Image>
+              {data.blue_bot.champ_name}
+            </div>
+            <div className="flex items-center gap-2">
+              <Image
+                src={`https://fpwrnfdqzvztmakmrdnc.supabase.co/storage/v1/object/public/champions_icons/${data.blue_sup.champ_name}.png`}
+                height={20}
+                width={20}
+                alt=""
+                className="border-cyan-500 bg-zinc-900 border rounded-md"
+              ></Image>
+              {data.blue_sup.champ_name}
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center ml-4 min-w-20">
+          <div className="flex flex-col gap-1">
+            <div className="text-red-500 uppercase text-sm text-right">
+              Red team
+            </div>
+            <div className="flex justify-end items-center gap-2">
+              {data.red_top.champ_name}
+              <Image
+                src={`https://fpwrnfdqzvztmakmrdnc.supabase.co/storage/v1/object/public/champions_icons/${data.red_top.champ_name}.png`}
+                height={20}
+                width={20}
+                alt=""
+                className="border-cyan-500 bg-zinc-900 border rounded-md"
+              ></Image>
+            </div>
+            <div className="flex justify-end items-center gap-2">
+              {data.red_jungler.champ_name}
+              <Image
+                src={`https://fpwrnfdqzvztmakmrdnc.supabase.co/storage/v1/object/public/champions_icons/${data.red_jungler.champ_name}.png`}
+                height={20}
+                width={20}
+                alt=""
+                className="border-cyan-500 bg-zinc-900 border rounded-md"
+              ></Image>
+            </div>
+            <div className="flex justify-end items-center gap-2">
+              {data.red_mid.champ_name}
+              <Image
+                src={`https://fpwrnfdqzvztmakmrdnc.supabase.co/storage/v1/object/public/champions_icons/${data.red_mid.champ_name}.png`}
+                height={20}
+                width={20}
+                alt=""
+                className="border-cyan-500 bg-zinc-900 border rounded-md"
+              ></Image>
+            </div>
+            <div className="flex justify-end items-center gap-2">
+              {data.red_bot.champ_name}
+              <Image
+                src={`https://fpwrnfdqzvztmakmrdnc.supabase.co/storage/v1/object/public/champions_icons/${data.red_bot.champ_name}.png`}
+                height={20}
+                width={20}
+                alt=""
+                className="border-cyan-500 bg-zinc-900 border rounded-md"
+              ></Image>
+            </div>
+            <div className="flex justify-end items-center gap-2">
+              {data.red_sup.champ_name}
+              <Image
+                src={`https://fpwrnfdqzvztmakmrdnc.supabase.co/storage/v1/object/public/champions_icons/${data.red_sup.champ_name}.png`}
+                height={20}
+                width={20}
+                alt=""
+                className="border-cyan-500 bg-zinc-900 border rounded-md"
+              ></Image>
+            </div>
+          </div>
         </div>
       </div>
     </div>
